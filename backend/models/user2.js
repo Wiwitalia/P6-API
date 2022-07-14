bf4d08a81746ea06799aa36a80de272184deabec
William Lebois <william.lebois@gmail.com>
2022-07-14 13:57:12 +0200

    Version final

---
+++ backend/models/user2.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+mongoose.connect('mongodb+srv://WilliamLebois:Kosovar93@cluster0.s10qhy0.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
